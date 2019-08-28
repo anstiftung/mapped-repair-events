2039ae86fe0daf37e060e3d5874cee65eb254c6e
Mario Rothauer <marothauer@gmail.com>
2019-08-28 22:02:15 +0200

    sort fix

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -117,8 +117,7 @@ class UsersController extends AppController
         
         $this->Skill = TableRegistry::getTableLocator()->get('Skills');
         $skillsForDropdown = $this->Skill->getForDropdown();
-        $skillsForDropdown['-1'] = 'Alle anzeigen';
-        asort($skillsForDropdown);
+        $skillsForDropdown = array_merge(['-1' => 'Alle anzeigen'], $skillsForDropdown);
         $this->set('skillsForDropdown', $skillsForDropdown);
         
         $metaTags = [
