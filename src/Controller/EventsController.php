7e96f1431795a8f0d8ee5e8afe4c771b26ab11b2
Mario Rothauer <marothauer@gmail.com>
2019-09-17 16:13:42 +0200

    workaround for notice

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -146,7 +146,9 @@ class EventsController extends AppController
                 }
             }
         }
-        
+        // workaround: if limit is not added, this warning appears
+        // Warning (2): count(): Parameter must be an array or an object that implements Countable in [src/Template/Events/my_events.ctp, line 42]
+        $workshops->limit(1000000);
         $this->set('workshops', $workshops);
         
         $metaTags = [
