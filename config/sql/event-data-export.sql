83fb7e06305f2eb5bb0e8cecfde32cbd9ea6cf54
Mario Rothauer <marothauer@gmail.com>
2019-10-09 08:53:13 +0200

    repair data export sql

---
+++ config/sql/event-data-export.sql
@@ -0,0 +1,13 @@
+SELECT
+    e.eventbeschreibung as Description,
+    w.name as Name,
+    CONCAT(e.strasse, ", ", e.zip, " ", e.ort) as Address,
+    e.ort as City,
+    e.veranstaltungsort as AdditionalLocationInfo,
+    CONCAT('https://www.reparatur-initiativen.de/', w.url, '?event=', e.uid, ',', e.datumstart) as Url,
+    e.lat,
+    e.lng
+FROM
+    events e
+JOIN workshops w ON e.workshop_uid = w.uid
+WHERE (e.datumstart BETWEEN '2019-10-18' AND '2019-10-20');
