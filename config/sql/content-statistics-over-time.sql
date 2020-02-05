c2f26ea3f9e88601be2a04239e1a7a145e80cc3c
Mario Rothauer <office@rothauer-it.com>
2020-02-05 13:52:05 +0100

    add content statistics overtime

---
+++ config/sql/content-statistics-over-time.sql
@@ -0,0 +1,26 @@
+SELECT COUNT( uid ) , DATE_FORMAT( created, '%m.%Y' ) dform,  DATE_FORMAT( created, '%Y-%m' ) d 
+FROM events 
+GROUP BY DATE_FORMAT( created, '%m.%Y' ) 
+ORDER BY d ASC LIMIT 0,100000;
+
+SELECT COUNT( uid ) , DATE_FORMAT( created, '%m.%Y' ) dform,  DATE_FORMAT( created, '%Y-%m' ) d
+FROM events
+GROUP BY DATE_FORMAT( created, '%m.%Y' ) ORDER BY d ASC LIMIT 0,100000;
+
+SELECT COUNT( uid ) , DATE_FORMAT( created, '%m.%Y' ) dform, DATE_FORMAT( created, '%Y-%m' ) d
+FROM workshops
+GROUP BY DATE_FORMAT( created, '%m.%Y' )
+ORDER BY d ASC LIMIT 0,100000;
+
+SELECT COUNT( id ) , DATE_FORMAT( created, '%m.%Y' ) dform, DATE_FORMAT( created, '%Y-%m' ) d
+FROM worknews
+WHERE confirm = 'ok'
+GROUP BY DATE_FORMAT( created, '%m.%Y' )
+ORDER BY d ASC LIMIT 0,100000;
+
+SELECT COUNT( id ) , DATE_FORMAT( created, '%m.%Y' ) dform, DATE_FORMAT( created, '%Y-%m' ) d
+FROM newsletters 
+WHERE 
+confirm = 'ok' 
+GROUP BY DATE_FORMAT( created, '%m.%Y' )
+ORDER BY d ASC LIMIT 0,100000;
