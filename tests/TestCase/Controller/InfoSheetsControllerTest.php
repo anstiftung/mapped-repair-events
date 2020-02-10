ef36206991e7d72ddeb3d2e757a20fe767d29782
Mario Rothauer <office@rothauer-it.com>
2020-02-10 12:10:38 +0100

    fixed tests

--- tests/TestCase/Controller/InfoSheetsControllerTest.php
+++ tests/TestCase/Controller/InfoSheetsControllerTest.php
@@ -82,7 +82,7 @@ class InfoSheetsControllerTest extends AppTestCase
                 'FormFieldOptions'
             ]
         ])->toArray();
-        $this->assertEquals(1, count($infoSheets));
+        $this->assertEquals(2, count($infoSheets));
         $this->assertEquals($infoSheets[0]->device_name, $this->newInfoSheetData['device_name']);
         $this->assertEquals($infoSheets[0]->defect_description, $this->newInfoSheetData['defect_description']);
         $this->assertEquals($infoSheets[0]->owner, 1);
