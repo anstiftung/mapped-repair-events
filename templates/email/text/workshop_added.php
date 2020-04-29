fba4a9862a8d37007e257926e5242e975bac021a
Mario Rothauer <office@rothauer-it.com>
2020-04-29 16:43:11 +0200

    add workshop detail url

--- templates/email/text/workshop_added.php
+++ templates/email/text/workshop_added.php
@@ -2,3 +2,5 @@
 Hallo,
 
 der Organisator <?php echo $username; ?> hat soeben die <?php echo Configure::read('AppConfig.initiativeNameSingular'); ?> <?php echo $workshop->name; ?> erstellt.
+
+<?php echo Configure::read('AppConfig.serverName') . $this->MyHtml->urlWorkshopDetail($workshop->url); ?>
