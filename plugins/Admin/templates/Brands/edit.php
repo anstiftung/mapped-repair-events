d5255bbccf773f5cf7c44fd5b91437f8c062a556
Mario Rothauer <office@rothauer-it.com>
2021-07-09 09:39:23 +0200

    unlock referer

--- plugins/Admin/templates/Brands/edit.php
+++ plugins/Admin/templates/Brands/edit.php
@@ -18,6 +18,7 @@ $this->element('addScript', array('script' =>
         <?php
             echo $this->element('heading', ['first' => $metaTags['title']]);
             echo $this->Form->hidden('referer', ['value' => $referer]);
+            $this->Form->unlockField('referer');
             echo $this->Form->control('Brands.name', ['label' => 'Name']).'<br />';
             echo $this->Form->control('Brands.status', ['type' => 'select', 'options' => Configure::read('AppConfig.approvalStatus')]).'<br />';
         ?>
