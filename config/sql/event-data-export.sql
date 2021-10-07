f35b6243f49045811fc701ca3f3924cdaeb6c70b
Mario Rothauer <office@rothauer-it.com>
2021-10-07 15:02:38 +0200

    update repair day sql

--- config/sql/event-data-export.sql
+++ config/sql/event-data-export.sql
@@ -1,17 +1,20 @@
 SELECT
     e.eventbeschreibung as Description,
+    e.datumstart as Date,
     w.name as Name,
     CONCAT(e.strasse, ", ", e.zip, " ", e.ort) as Address,
     e.ort as City,
     e.veranstaltungsort as AdditionalLocationInfo,
     CONCAT('https://www.reparatur-initiativen.de/', w.url, '?event=', e.uid, ',', e.datumstart) as Url,
     e.lat,
-    e.lng
+    e.lng,
+    e.is_online_event as Online
 FROM
     events e
 JOIN workshops w ON e.workshop_uid = w.uid
 WHERE
-    (e.datumstart BETWEEN '2019-10-18' AND '2019-10-20')
+    (e.datumstart BETWEEN '2021-10-11' AND '2021-10-21')
 AND e.status = 1
 AND w.status = 1
+ORDER BY e.datumstart ASC
 ;
