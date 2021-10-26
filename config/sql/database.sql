34ae833dfead836f9ce9dc53f800228a3cd7edb1
Mario Rothauer <office@rothauer-it.com>
2021-10-26 17:28:18 +0200

    Revert "fix: testAddEventValidations"
    This reverts commit 64cca38df78ac1cc984e07e18399d6eefffca185.

--- config/sql/database.sql
+++ config/sql/database.sql
@@ -88,6 +88,14 @@ CREATE TABLE `blogs` (
   `url` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Daten für Tabelle `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `name`, `text`, `url`) VALUES
+(1, 'Aktuelles', '', 'neuigkeiten'),
+(2, 'Kategorie 2', '', 'kategorie-2');
+
 -- --------------------------------------------------------
 
 --
