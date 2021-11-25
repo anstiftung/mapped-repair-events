2de2f8688e4a0c204760b7ef0c31e09b3c93a010
Mario Rothauer <office@rothauer-it.com>
2021-11-25 11:19:47 +0100

    correct sort

--- src/Controller/SkillsController.php
+++ src/Controller/SkillsController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controller;
 
+use App\Controller\Component\StringComponent;
+use Cake\Core\Configure;
 use Cake\Event\EventInterface;
 
 class SkillsController extends AppController
@@ -42,14 +44,46 @@ class SkillsController extends AppController
             ]
         ]);
 
+        $this->Category = $this->getTableLocator()->get('Categories');
+        $categories = $this->Category->find('all', [
+            'order' => [
+                'Categories.name'=> 'ASC'
+            ],
+            'conditions' => [
+                'Categories.parent_id IS NULL',
+                'Categories.status' => APP_ON,
+                'Categories.visible_on_platform' => APP_ON,
+            ],
+            'contain' => [
+                'Users' => [
+                    'conditions' => [
+                        'FIND_IN_SET("categories", private) = ' => 0, // Users.private would be converted to users.private on prod so leave it out!
+                        'Users.status' => APP_ON,
+                    ]
+                ]
+            ]
+        ]);
+
         $preparedSkills = [];
         $skillCount = 0;
+        foreach($categories as $category) {
+            if (count($category->users) > 0) {
+                $category->url = Configure::read('AppConfig.htmlHelper')->urlUsers($category->name);
+                $preparedSkills[strtoupper(substr($category->name, 0, 1))][] = $category;
+                $skillCount++;
+            }
+        }
+
         foreach($skills as $skill) {
             if (count($skill->users) > 0) {
+                $skill->url = Configure::read('AppConfig.htmlHelper')->urlSkillDetail($skill->id, StringComponent::slugify($skill->name));
                 $preparedSkills[strtoupper(substr($skill->name, 0, 1))][] = $skill;
                 $skillCount++;
             }
         }
+
+        ksort($preparedSkills, SORT_STRING);
+
         $this->set('skills', $preparedSkills);
         $this->set('skillCount', $skillCount);
     }
