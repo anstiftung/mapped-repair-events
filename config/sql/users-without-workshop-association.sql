91e58b49c76f23b326c043fc099e90a560f1c1cd
Mario Rothauer <office@rothauer-it.com>
2022-01-27 09:52:41 +0100

    exclude deleted users

--- config/sql/users-without-workshop-association.sql
+++ config/sql/users-without-workshop-association.sql
@@ -2,5 +2,6 @@ SELECT u.uid, u.nick, u.firstname, u.lastname, u.email, u.created
 FROM users u
 LEFT JOIN users_workshops uw ON uw.user_uid = u.uid
 WHERE uw.workshop_uid IS NULL
+AND u.status > -1
 AND u.created <  '2019-01-01'
 LIMIT 0 , 100000
