56d972fef1e162786da067ad4af5deb40bba97af
Mario Rothauer <office@rothauer-it.com>
2022-09-26 13:59:00 +0200

    update

--- config/sql/event-data-export.sql
+++ config/sql/event-data-export.sql
@@ -13,8 +13,9 @@ FROM
     events e
 JOIN workshops w ON e.workshop_uid = w.uid
 WHERE
-    (e.datumstart BETWEEN '2021-10-11' AND '2021-10-21')
+    (e.datumstart BETWEEN '2022-10-10' AND '2022-10-20')
 AND e.status = 1
 AND w.status = 1
 ORDER BY e.datumstart ASC
+LIMIT 10000
 ;
