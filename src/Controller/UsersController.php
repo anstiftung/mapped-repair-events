079d0a3dcbbb1e84cfaa999ddbdae68cbb5b4e3e
Mario Rothauer <office@rothauer-it.com>
2022-09-28 11:25:02 +0200

    bugfix

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -626,11 +626,10 @@ class UsersController extends AppController
             $user = $this->User->patchEntity($user, $this->request->getData(), ['validate' => 'Registration']);
 
             if (!$this->isCalledByTestSuite()) {
+                pr($this->request->getSession()->read('captchaPhrase'));
                 $captchaBuilder->setPhrase($this->request->getSession()->read('captchaPhrase'));
                 if (!$captchaBuilder->testPhrase($this->request->getData('Users.captcha'))) {
-                    if(!$this->request->getData('Users.reload_captcha')) {
-                        $user->setError('captcha', 'Das Captcha ist nicht korrekt.');
-                    }
+                    $user->setError('captcha', 'Das Captcha ist nicht korrekt.');
                 }
                 if($this->request->getData('Users.reload_captcha')) {
                     $this->request = $this->request->withData('Users.captcha', '');
