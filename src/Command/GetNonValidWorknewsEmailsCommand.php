be7220fd07d576718a9e505e9c6b1e9a52df391b
mrothauer <office@rothauer-it.com>
2023-01-17 11:51:16 +0100

    remove wrong comments

--- src/Command/GetNonValidWorknewsEmailsCommand.php
+++ src/Command/GetNonValidWorknewsEmailsCommand.php
@@ -17,12 +17,10 @@ class GetNonValidWorknewsEmailsCommand extends Command
 
         $this->Worknews = $this->getTableLocator()->get('Worknews');
         
-        // find all events that start in one week
         $worknews = $this->Worknews->find('all', [
             'order' => ['Worknews.id' => 'ASC'],
         ]);
 
-        // send notification mail to all subscribers
         $i = 0;
         foreach($worknews as $w) {
             $i++;
