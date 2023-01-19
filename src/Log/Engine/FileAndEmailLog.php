a0c7cb9fcc969a010e63a3da58aebd869aaf912a
mrothauer <office@rothauer-it.com>
2023-01-19 19:48:33 +0100

    exclude InvalidCsrfTokenException

--- src/Log/Engine/FileAndEmailLog.php
+++ src/Log/Engine/FileAndEmailLog.php
@@ -23,7 +23,7 @@ class FileAndEmailLog extends FileLog
     {
 
         $ignoredExceptionsRegex = [
-           '(MissingController|MissingAction|RecordNotFound|NotFound)Exception',
+           '(MissingController|MissingAction|RecordNotFound|NotFound|InvalidCsrfToken)Exception',
            'Workshops\/rss\/home\.ctp',
            'RssHelper is deprecated',
            'UsersController::publicProfile()',
