61acffd1d50a5419d1b58bfdbc4f582c05c35c30
mrothauer <office@rothauer-it.com>
2023-03-22 08:18:57 +0100

    extract email signature

---
+++ templates/element/emailSignature.php
@@ -0,0 +1,12 @@
+--
+
+Netzwerk Reparatur-Initiativen
+Tom Hansing | Ina Hemmelmann
+
+Tel: +49 089 74 74 60 -18
+
+E-Mail: reparieren@anstiftung.de
+
+
+anstiftung, gemeinnützige Stiftung bürgerlichen Rechts, Daiserstraße 15, Rgb., 81371 München, www.anstiftung.de
+Vorstand: Dr. Christa Müller (Vorsitz), Daniel Überall
