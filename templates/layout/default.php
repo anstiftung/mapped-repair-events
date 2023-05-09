92c01202a168ee355338bd17834e75a5378a1c02
mrothauer <office@rothauer-it.com>
2023-05-09 09:30:20 +0200

    fix url

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -45,11 +45,11 @@ use Cake\Utility\Inflector;
 <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-<link rel="manifest" href="/img/site.webmanifest">
+<link rel="manifest" href="/site.webmanifest">
 <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
 <link rel="shortcut icon" href="/img/favicon.ico">
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-config" content="/img/browserconfig.xml">
+<meta name="msapplication-config" content="/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 
 <meta name="application-name" content="<?php echo Configure::read('AppConfig.serverName'); ?>"/>
