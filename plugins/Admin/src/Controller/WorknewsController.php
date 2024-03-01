28bbabe2a30c5ac0bd75c2b1544a223c72fb862c
mrothauer <office@rothauer-it.com>
2024-03-01 14:08:09 +0100

    email filter

--- plugins/Admin/src/Controller/WorknewsController.php
+++ plugins/Admin/src/Controller/WorknewsController.php
@@ -1,17 +1,39 @@
 <?php
 namespace Admin\Controller;
 
-use App\Model\Table\WorknewsTable;
+use Cake\Event\EventInterface;
 
 class WorknewsController extends AdminAppController
 {
+
+    public function beforeFilter(EventInterface $event)
+    {
+        $this->searchUid = false;
+        $this->searchName = false;
+        $this->searchText = false;
+        $this->searchStatus = false;
+
+        $this->addSearchOptions([
+            'Worknews.email' => [
+                'name' => 'Worknews.email',
+                'searchType' => 'search'
+            ],
+        ]);
+
+        parent::beforeFilter($event);
+
+    }
 
     public function index()
     {
         parent::index();
 
+        $conditions = [];
+        $conditions = array_merge($this->conditions, $conditions);
+
         $worknewsTable = $this->getTableLocator()->get('Worknews');
-        $query = $worknewsTable->find('all',
+        $query = $worknewsTable->find('all',#
+        conditions: $conditions,
         contain: [
             'Workshops',
         ]);
