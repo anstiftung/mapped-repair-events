42679de3f2a5ca94c924fcd9b00ae1daf6ad00d2
mrothauer <office@rothauer-it.com>
2024-06-11 16:34:56 +0200

    cleanup 60 days

--- config/app_queue.php
+++ config/app_queue.php
@@ -15,7 +15,7 @@ return [
         'workertimeout' => 360, // cron starts new worker every 5 min (=300 sec), overlap
         'workermaxruntime' => 360,
         'sleeptime' => 5,
-        'cleanuptimeout' => 518400, // 6 days
+        'cleanuptimeout' => 60 * 60 * 24 * 60, // 60 days
         'gcprob' => 100,
         'connection' => $connection,
         'exitwhennothingtodo' => $exitwhennothingtodo,
