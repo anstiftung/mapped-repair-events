743fe6ce543d58d2e594c2de6eeed1cdfab7e1e3
mrothauer <office@rothauer-it.com>
2024-09-11 11:18:51 +0200

    never show logo on mobile

--- templates/element/core/header.php
+++ templates/element/core/header.php
@@ -12,7 +12,7 @@ use Cake\Core\Configure;
 
 <div id="claim"><?php echo Configure::read('AppConfig.claim'); ?></div>
 
-<?php if (empty($loggedUser)) { ?>
+<?php if (!$this->request->getSession()->read('isMobile') && empty($loggedUser)) { ?>
     <a id="anstiftung-logo" href="https://anstiftung.de/praxis/reparieren" title="anstiftung - Offene Werkstätten, Interkulturelle und Urbane Gemeinschaftsgärten" target="_blank">
     <img alt="Logo anstiftung" src="/img/anstiftungslogo-farbig-72dpi.jpg" />
     </a>
