f52e3293892fc250b129ba359da5dc8f72ec6ad8
mrothauer <office@rothauer-it.com>
2024-11-28 20:34:04 +0100

    status is no var_char

--- config/Migrations/20241128191908_Fundings5.php
+++ config/Migrations/20241128191908_Fundings5.php
@@ -9,5 +9,7 @@ class Fundings5 extends AbstractMigration
     {
         $sql = "ALTER TABLE `fundingsupporters` ADD bic varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL AFTER `iban`;";
         $this->execute($sql);
+        $sql = "ALTER TABLE `fundings` CHANGE `status` `status` INT UNSIGNED NULL DEFAULT NULL;";
+        $this->execute($sql);
     }
 }
