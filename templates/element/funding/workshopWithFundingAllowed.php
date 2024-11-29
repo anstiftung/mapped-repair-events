d63a6b7b5cdd664ac10fee6cba7e7027cc66b81f
mrothauer <office@rothauer-it.com>
2024-11-29 08:56:53 +0100

    fix

--- templates/element/funding/workshopWithFundingAllowed.php
+++ templates/element/funding/workshopWithFundingAllowed.php
@@ -40,7 +40,7 @@ echo '<div class="workshop-wrapper">';
                 echo $this->element('funding/activityProof', ['workshop' => $workshop]);
                 echo $this->element('funding/freistellungsbescheid', ['workshop' => $workshop]);
             } else {
-                echo $this->element('funding/submitInfo', ['funding' => $funding->workshop_funding]);
+                echo $this->element('funding/submitInfo', ['funding' => $workshop->workshop_funding]);
            }
             if ($workshop->funding_exists && !$workshop->workshop_funding->is_submitted) {
                 echo $this->element('funding/delete', ['funding' => $workshop->workshop_funding]);
