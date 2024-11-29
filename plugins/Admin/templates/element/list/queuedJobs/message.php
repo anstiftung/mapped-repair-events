43a3b7683d0968080f7d5c9affd6fe7127d101cf
mrothauer <office@rothauer-it.com>
2024-11-29 10:52:37 +0100

    show attachments of email queue

--- plugins/Admin/templates/element/list/queuedJobs/message.php
+++ plugins/Admin/templates/element/list/queuedJobs/message.php
@@ -2,4 +2,4 @@
 $data = json_decode($object->data);
 ?>
 
-<iframe srcdoc='<?php echo $data->settings->htmlMessage; ?>' style="width: 800px; height: 600px; border: none;"></iframe>
+<iframe srcdoc='<?php echo $data->settings->htmlMessage; ?>' style="width: 770px; height: 600px; border: none;"></iframe>
