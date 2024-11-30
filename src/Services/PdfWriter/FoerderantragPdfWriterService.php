60099495b36f4924bb535579850bae1f92449bbb
mrothauer <office@rothauer-it.com>
2024-11-30 17:06:27 +0100

    change filename

--- src/Services/PdfWriter/FoerderantragPdfWriterService.php
+++ src/Services/PdfWriter/FoerderantragPdfWriterService.php
@@ -8,8 +8,6 @@ use App\Services\Pdf\AppTcpdfService;
 use App\Services\PdfWriter\PdfWriterService;
 use App\Model\Entity\Fundingupload;
 use Cake\Datasource\FactoryLocator;
-use Cake\Core\Configure;
-use Cake\I18n\DateTime;
 
 class FoerderantragPdfWriterService extends PdfWriterService
 {
@@ -25,7 +23,7 @@ class FoerderantragPdfWriterService extends PdfWriterService
         $fundingsTable = FactoryLocator::get('Table')->get('Fundings');
         $funding = $fundingsTable->getUnprivatizedFundingWithAllAssociations($fundingUid);
 
-        $filename = 'Foerderantrag_' . $funding->uid . '_' . $timestamp->i18nFormat('yyyyMMdd_HHmmss') . '.pdf';
+        $filename = 'Foerderantrag_anstiftung_bmuv_' . $funding->uid . '_' . $timestamp->i18nFormat('yyyyMMdd_HHmmss') . '.pdf';
         $this->setFilename(Fundingupload::UPLOAD_PATH . $funding->uid . DS . 'attachments' . DS . $filename);
         
         $blocks = [
--- src/Services/PdfWriter/FoerderbewilligungPdfWriterService.php
+++ src/Services/PdfWriter/FoerderbewilligungPdfWriterService.php
@@ -7,8 +7,6 @@ use App\Services\Pdf\AppTcpdfService;
 use App\Services\PdfWriter\PdfWriterService;
 use App\Model\Entity\Fundingupload;
 use Cake\Datasource\FactoryLocator;
-use Cake\Core\Configure;
-use Cake\I18n\DateTime;
 
 class FoerderbewilligungPdfWriterService extends PdfWriterService
 {
@@ -24,7 +22,7 @@ class FoerderbewilligungPdfWriterService extends PdfWriterService
         $fundingsTable = FactoryLocator::get('Table')->get('Fundings');
         $funding = $fundingsTable->getUnprivatizedFundingWithAllAssociations($fundingUid);
 
-        $filename = 'Foerderbewilligung_' . $funding->uid . '_' . $timestamp->i18nFormat('yyyyMMdd_HHmmss') . '.pdf';
+        $filename = 'Foerderbewilligung_anstiftung_bmuv_' . $funding->uid . '_' . $timestamp->i18nFormat('yyyyMMdd_HHmmss') . '.pdf';
         $this->setFilename(Fundingupload::UPLOAD_PATH . $funding->uid . DS . 'attachments' . DS . $filename);
         
         $this->setData([
