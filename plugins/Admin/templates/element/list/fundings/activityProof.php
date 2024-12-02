f73e4a3e6ef90183614ad1ea5b4a57848debf1cd
mrothauer <office@rothauer-it.com>
2024-12-02 23:22:16 +0100

    add nonbraeking spaces

--- plugins/Admin/templates/element/list/fundings/activityProof.php
+++ plugins/Admin/templates/element/list/fundings/activityProof.php
@@ -2,6 +2,6 @@
 echo '<div style="padding:5px;" class="'. $object->activity_proof_status_css_class .'">';
     echo $object->workshop->funding_activity_proof_required ? 'Ja' : 'Nein';
     if ($object->workshop->funding_activity_proof_required) {
-        echo ' /  ' . $object->activity_proofs_count . 'x';
+        echo ' / ' . $object->activity_proofs_count . 'x';
     }
 echo '</div>';
