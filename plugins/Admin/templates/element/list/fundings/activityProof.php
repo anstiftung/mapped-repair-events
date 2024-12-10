edb5608f0cf62e9185dc0af256c8fe9e5042affd
mrothauer <office@rothauer-it.com>
2024-12-10 14:35:19 +0100

    cleaner

--- plugins/Admin/templates/element/list/fundings/activityProof.php
+++ plugins/Admin/templates/element/list/fundings/activityProof.php
@@ -1,7 +1,6 @@
 <?php
 echo '<div style="padding:5px;" class="'. $object->activity_proof_status_css_class .'">';
-    echo $object->workshop->funding_activity_proof_required ? 'Ja' : 'Nein';
     if ($object->workshop->funding_activity_proof_required) {
-        echo ' / ' . $object->activity_proofs_count . 'x';
+        echo $object->activity_proofs_count . 'x';
     }
 echo '</div>';
