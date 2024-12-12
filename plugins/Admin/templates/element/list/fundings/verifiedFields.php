f8bd0fd5ff355c942dbf1f1ce7d062985440c801
mrothauer <office@rothauer-it.com>
2024-12-12 17:05:46 +0100

    red if no field verified

--- plugins/Admin/templates/element/list/fundings/verifiedFields.php
+++ plugins/Admin/templates/element/list/fundings/verifiedFields.php
@@ -1,4 +1,10 @@
 <?php
-echo '<div style="padding:5px;text-align:right;border-radius:3px;" class="'. ($object->user_fields_verified ? 'is-verified' : 'is-pending') .'">';
+$class = $object->user_fields_verified ? 'is-verified' : 'is-pending';
+
+if ($object->user_fields_verified_count == 0) {
+    $class = 'is-missing';
+}
+
+echo '<div style="padding:5px;text-align:right;border-radius:3px;" class="'. $class .'">';
     echo $object->user_fields_verified_count . ' / ' . $object->user_fields_count;
 echo '</div>';
