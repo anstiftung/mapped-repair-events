bf2ec4bc49253412f2fd892a5f25a398cb9146cd
mrothauer <office@rothauer-it.com>
2024-12-17 10:56:59 +0100

    fixes

--- templates/element/funding/moneyTransferInfo.php
+++ templates/element/funding/moneyTransferInfo.php
@@ -2,8 +2,10 @@
 
 use Cake\Core\Configure;
 
+if ($funding->money_transfer_date === null) {
+    return;
+}
+
 echo '<div class="verification-wrapper is-verified">';
-    if ($funding->money_transfer_date !== null) {
-        echo 'Am ' . $funding->money_transfer_date->i18nFormat(Configure::read('DateFormat.de.DateShort')) . ' wurden ' . $this->MyNumber->formatAsDecimal($funding->budgetplan_total_with_limit) . ' € überwiesen.';
-    }
+    echo 'Am ' . $funding->money_transfer_date->i18nFormat(Configure::read('DateFormat.de.DateShort')) . ' wurden ' . $this->MyNumber->formatAsDecimal($funding->budgetplan_total_with_limit) . ' € überwiesen.';
 echo '</div>';
