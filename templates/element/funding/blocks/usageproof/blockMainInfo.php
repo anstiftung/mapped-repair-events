7760c83d0beea2fea2adbd20190c56eac491d898
mrothauer <office@rothauer-it.com>
2025-02-10 15:33:46 +0100

    show admin status in form

--- templates/element/funding/blocks/usageproof/blockMainInfo.php
+++ templates/element/funding/blocks/usageproof/blockMainInfo.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
+
+echo $this->element('funding/status/usageproofStatus', [
+    'funding' => $funding,
+    'additionalTextBefore' => '',
+]);
 ?>
+
 <p style="margin-bottom:20px;">
     Sachbericht und Belegliste müssen korrekt ausgefüllt werden, bis beide Blöcke grün sind.<br />
     Anschließend wird der Verwendungsnachweis von einem Admin bestätigt.
