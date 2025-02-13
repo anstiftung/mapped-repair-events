2d3a79e4981202ff0315cb23eabb1882f0dd8afd
mrothauer <office@rothauer-it.com>
2025-02-13 10:49:05 +0100

    wording

--- templates/element/funding/blocks/usageproof/blockMainInfo.php
+++ templates/element/funding/blocks/usageproof/blockMainInfo.php
@@ -3,6 +3,6 @@ declare(strict_types=1);
 ?>
 
 <p style="margin-bottom:20px;">
-    Sachbericht und Belegliste müssen korrekt ausgefüllt werden, bis beide Blöcke grün sind.<br />
-    Anschließend wird der Verwendungsnachweis von einem Admin bestätigt.
+    Sachbericht, Belegliste und Bestätigungen müssen korrekt ausgefüllt werden, bis alle Blöcke grün sind.<br />
+    Anschließend kann der Verwendungsnachweis eingereicht werden und wird anschließend von einem Admin bestätigt.
 </p>
