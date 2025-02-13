67d3a1f3b76d1a3efa000481782db483df4a3b27
mrothauer <office@rothauer-it.com>
2025-02-13 10:47:28 +0100

    hide admin status

--- templates/element/funding/usageproofInfo.php
+++ templates/element/funding/usageproofInfo.php
@@ -7,14 +7,15 @@ if ($funding->money_transfer_date === null) {
     return;
 }
 
+/*
 echo $this->element('funding/status/usageproofStatus', [
     'funding' => $funding,
     'additionalTextBefore' => 'Verwendungsnachweis: ',
 ]);
+*/
 
-if ($funding->usageproof_status != Funding::STATUS_VERIFIED_BY_ADMIN) {
+//if ($funding->usageproof_status != Funding::STATUS_VERIFIED_BY_ADMIN) {
     echo '<div style="margin-top:10px;margin-left:10px;">';
-        echo '<span style="margin-left:5px;margin-right:5px;"><i class="fa-solid fa-arrow-turn-up fa-rotate-90"></i></span>';
         echo $this->Html->link(
             'Verwendungsnachweis ' . ($funding->usageproof_exists ? 'bearbeiten' : 'erstellen'),
             $this->Html->urlFundingsUsageproof($funding->uid),
@@ -23,4 +24,4 @@ if ($funding->usageproof_status != Funding::STATUS_VERIFIED_BY_ADMIN) {
             ],
         );
    echo '</div>';
-}
+//}
