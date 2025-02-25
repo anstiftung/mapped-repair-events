1469c1722381a15e727ef7827ecbded80244d9d1
mrothauer <office@rothauer-it.com>
2025-02-25 13:45:38 +0100

    add zwischensummen

--- templates/element/funding/blocks/usageproof/blockReceiptlistTotals.php
+++ templates/element/funding/blocks/usageproof/blockReceiptlistTotals.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use App\Model\Entity\Funding;
+use App\Model\Entity\Fundingbudgetplan;
 
 $this->element('addScript', ['script' =>
     JS_NAMESPACE.".Funding.bindReceiptlistCheckboxA();".
@@ -9,10 +10,13 @@ $this->element('addScript', ['script' =>
 ]);
 
 echo '<div class="total-wrapper">';
-    echo '<p class="total">Fördersumme: ' . $this->MyNumber->formatAsDecimal($funding->budgetplan_total_with_limit) . ' €</p>';
+    echo '<p style="margin-bottom:5px;" class="total">Fördersumme: ' . $this->MyNumber->formatAsDecimal($funding->budgetplan_total_with_limit) . ' €</p>';
+    foreach($funding->grouped_valid_receiptlists as $typeId => $fundingreceiptlists) {
+        echo '<p style="font-size:1.1em;" class="total">Summe (' . Fundingbudgetplan::TYPE_MAP[$typeId] . '): '. $this->MyNumber->formatAsDecimal($funding->grouped_valid_receiptlists_totals[$typeId]) . ' €</p>';
+    }
     echo '<p class="total">Belegte Gesamtsumme: ' . $this->MyNumber->formatAsDecimal($funding->receiptlist_total) . ' €</p>';
     if ($funding->receiptlist_difference > 0) {
-        echo '<p class="total">Restbetrag: <b>' . $this->MyNumber->formatAsDecimal($funding->receiptlist_difference) . ' €</b></p>';
+        echo '<p style="margin-top:5px;" class="total">Restbetrag: <b>' . $this->MyNumber->formatAsDecimal($funding->receiptlist_difference) . ' €</b></p>';
     }
 echo '</div>';
 
