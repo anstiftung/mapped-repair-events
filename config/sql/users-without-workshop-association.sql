c460d06fa932103f27955bf752156f8423bc64f1
mrothauer <office@rothauer-it.com>
2025-03-03 21:10:36 +0100

    updaet

--- config/sql/users-without-workshop-association.sql
+++ config/sql/users-without-workshop-association.sql
@@ -1,7 +1,7 @@
-SELECT u.uid, u.nick, u.firstname, u.lastname, u.email, u.created
+SELECT u.uid, u.nick, u.firstname, u.lastname, u.email, u.created, u.status
 FROM users u
 LEFT JOIN users_workshops uw ON uw.user_uid = u.uid
 WHERE uw.workshop_uid IS NULL
 AND u.status > -1
-AND u.created <  '2019-01-01'
+AND u.created <  '2023-01-01'
 LIMIT 0 , 100000
