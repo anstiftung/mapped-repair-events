5cb55f52f7b29630319dbb7413f511ff40c5c4d5
mrothauer <office@rothauer-it.com>
2025-03-07 11:40:24 +0100

    show only if checkbox is checked

--- plugins/Admin/templates/element/list/fundings/receiptlistDifference.php
+++ plugins/Admin/templates/element/list/fundings/receiptlistDifference.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-if ($object->fundingusageproof && $object->receiptlist_difference > 0) {
+if ($object->fundingusageproof && $object->fundingusageproof->payback_ok && $object->receiptlist_difference > 0) {
     echo '<div style="text-align:right;border-radius:3px;">';
         echo $this->MyNumber->formatAsDecimal($object->receiptlist_difference) . ' €';
     echo '</div>';
