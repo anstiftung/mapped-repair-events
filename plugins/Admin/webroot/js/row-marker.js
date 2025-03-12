57613a2c6c88321fe774465e90a973d2c2838549
mrothauer <office@rothauer-it.com>
2025-03-12 10:05:46 +0100

    background changes on select

--- plugins/Admin/webroot/js/row-marker.js
+++ plugins/Admin/webroot/js/row-marker.js
@@ -1,22 +1,33 @@
 MappedRepairEvents.RowMarker = {
 
     init: () => {
-        var rowMarkerAll = $('input#row-marker-all').on('click', function () {
-            var row;
+        const rowMarkerCheckboxSelector = 'input.row-marker[type="checkbox"]';
+
+        $('input#row-marker-all').on('click', function () {
+            let row;
             if (this.checked) {
-                row = $('input.row-marker[type="checkbox"]:not(:checked):not(:disabled)');
+                row = $(rowMarkerCheckboxSelector + ':not(:checked):not(:disabled)');
                 if (row.closest('tr').css('display') != 'none') {
                     row.prop('checked', true);
                     row.closest('tr').addClass('selected');
                 }
             } else {
-                row = $('input.row-marker[type="checkbox"]:checked');
+                row = $(rowMarkerCheckboxSelector + ':checked');
                 row.prop('checked', false);
                 row.closest('tr').removeClass('selected');
             }
         });
+
+        // change color of row on click of checkbox
+        $('table.list').find(rowMarkerCheckboxSelector).on('click', function () {
+            const row = $(this).closest('tr');
+            if (row.hasClass('selected')) {
+                row.removeClass('selected');
+            } else {
+                row.addClass('selected');
+            }
+        });
     
-        return rowMarkerAll;
     },
 
 };
