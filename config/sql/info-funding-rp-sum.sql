0a9d06ca9da84753156944d4435897dcb31d8ebd
mrothauer <office@rothauer-it.com>
2025-04-30 10:13:40 +0200

    add least

--- config/sql/info-funding-rp-sum.sql
+++ config/sql/info-funding-rp-sum.sql
@@ -1,6 +1,10 @@
-SELECT SUM(fbp.amount)
+SELECT
+funding_uid,
+workshop_uid
+LEAST(SUM(fbp.amount), 3000) as sum_least
 FROM fundingbudgetplans fbp
 JOIN fundings f ON fbp.funding_uid = f.uid
 JOIN workshops w ON f.workshop_uid = w.uid
 WHERE w.province_id = 11
 AND fbp.type > 0 AND fbp.description <> '' AND fbp.amount > 0
+GROUP BY funding_uid
