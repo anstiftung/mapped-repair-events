fe949aae11c84d9681fa6256c86110b0a9b2dc12
mrothauer <office@rothauer-it.com>
2025-05-15 14:38:41 +0200

    delete user script

--- config/sql/users-without-workshop-association.sql
+++ config/sql/users-without-workshop-association.sql
@@ -1,7 +1,7 @@
-SELECT u.uid, u.nick, u.firstname, u.lastname, u.email, u.created, u.status
+SELECT u.uid, u.nick, u.firstname, u.lastname, u.email, u.created, u.updated, u.status
 FROM users u
 LEFT JOIN users_workshops uw ON uw.user_uid = u.uid
 WHERE uw.workshop_uid IS NULL
-AND u.status > -1
-AND u.created <  '2023-01-01'
+AND u.created < '2023-01-01'
+AND u.updated < '2024-01-01'
 LIMIT 0 , 100000
