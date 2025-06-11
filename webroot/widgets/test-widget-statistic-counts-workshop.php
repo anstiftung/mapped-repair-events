33ef4b94feaf34d7bbb2a1637f224dbd4f0c974c
mrothauer <office@rothauer-it.com>
2025-06-11 22:06:08 +0200

    adapt examples

--- webroot/widgets/test-widget-statistic-counts-workshop.php
+++ webroot/widgets/test-widget-statistic-counts-workshop.php
@@ -14,13 +14,15 @@
     </head>
 
     <body>
-        <iframe style="height:18px;width:500px;" src="/widgets/statistics-counts-global"></iframe>
+        <iframe style="height:18px;width:520px;" src="/widgets/statistics-counts-global"></iframe>
         <br />
         <iframe style="height:130px;width:200px;" src="/widgets/statistics-counts-workshop/11277"></iframe>
         <br />
-        <iframe style="height:18px;width:500px;" src="/widgets/statistics-counts-workshop/11277?showWorkshopName=0"></iframe>
+        <iframe style="height:18px;width:520px;" src="/widgets/statistics-counts-workshop/11277?showWorkshopName=0"></iframe>
         <br />
-        <iframe style="height:50px;width:500px;" src="/widgets/statistics-counts-workshop/11277?borderColorOk=rgb(145,145,145)&backgroundColorOk=rgba(145,145,145,0.6)&borderColorNotOk=rgb(201,183,183)&backgroundColorNotOk=rgba(201,183,183,0.6)&borderColorRepairable=rgb(242,217,164)&backgroundColorRepairable=rgba(242,217,164,0.6)"></iframe>
+        <iframe style="height:70px;width:200px;" src="/widgets/statistics-counts-workshop/5457?showWorkshopName=0"></iframe>
+        <br />
+        <iframe style="height:50px;width:520px;" src="/widgets/statistics-counts-workshop/11277?borderColorOk=rgb(145,145,145)&backgroundColorOk=rgba(145,145,145,0.6)&borderColorNotOk=rgb(201,183,183)&backgroundColorNotOk=rgba(201,183,183,0.6)&borderColorRepairable=rgb(242,217,164)&backgroundColorRepairable=rgba(242,217,164,0.6)"></iframe>
     </body>
 
 </html>
