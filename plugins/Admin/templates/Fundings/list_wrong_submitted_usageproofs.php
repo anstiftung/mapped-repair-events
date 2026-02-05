50d6aa87f2924a3d4762a7a1c4274c99c7a23ba3
mrothauer <office@rothauer-it.com>
2026-02-05 09:39:52 +0100

    phpstan fix

--- plugins/Admin/templates/Fundings/list_wrong_submitted_usageproofs.php
+++ plugins/Admin/templates/Fundings/list_wrong_submitted_usageproofs.php
@@ -1,6 +1,7 @@
 <?php
+declare(strict_types=1);
 
-        echo 'Falsch eingereichte Verwendungsnachweise: ' . count($errorFundings) . '<br /><br />';
-        foreach($errorFundings as $errorFunding) {
-            echo 'FundingUID: ' . $errorFunding->uid . ' - ' . $errorFunding->workshop->name . '<br />';
-        }
+echo 'Falsch eingereichte Verwendungsnachweise: ' . count($errorFundings) . '<br /><br />';
+foreach($errorFundings as $errorFunding) {
+    echo 'FundingUID: ' . $errorFunding->uid . ' - ' . $errorFunding->workshop->name . '<br />';
+}
