94ece403e610c7e39973823a1a332dc54e611718
mrothauer <office@rothauer-it.com>
2026-07-03 14:45:08 +0200

    do not check w status

--- config/sql/fundings/events-count-of-funded-workshops.sql
+++ config/sql/fundings/events-count-of-funded-workshops.sql
@@ -4,7 +4,6 @@ FROM events e
 INNER JOIN workshops w ON w.uid = e.workshop_uid
 INNER JOIN fundings f ON f.workshop_uid = w.uid
 WHERE e.status = 1
-AND w.status = 1
-AND e.datumstart BETWEEN '2026-03-31' AND '2026-06-30'
+AND e.datumstart BETWEEN '2026-01-01' AND '2026-12-31'
 #AND e.datumstart BETWEEN '2025-03-31' AND '2026-03-31'
 ;
