1b1db9c82c9152621bf1689e2b1c2885712c4522
mrothauer <office@rothauer-it.com>
2026-07-29 10:51:59 +0200

    add link with filter

---
+++ plugins/Admin/templates/element/list/categories/infoSheetCount.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+echo $this->Html->link((string)$object->info_sheet_count, '/admin/info-sheets/index?' . http_build_query([
+    'key-standard' => 'Categories.name',
+    'val-standard' => $object->name,
+]));
