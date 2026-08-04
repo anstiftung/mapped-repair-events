267d9304a2febec7d1825551e63cf099d7581eb2
mrothauer <office@rothauer-it.com>
2026-08-04 11:33:25 +0200

    is workshop available check

--- plugins/Admin/templates/element/list/infoSheets/workshopName.php
+++ plugins/Admin/templates/element/list/infoSheets/workshopName.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+if (!$object->event || !$object->event->workshop) {
+    return;
+}
+
 echo $this->Html->link((string)$object->event->workshop->name, '/admin/info-sheets/index?' . http_build_query([
     'key-standard' => 'Events.workshop_uid',
     'val-standard' => $object->event->workshop->uid,
